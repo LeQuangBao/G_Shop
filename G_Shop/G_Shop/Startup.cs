e706b7a9b578057933ab84d1bce023f35e8218e4
Lê Quang Bảo <lequangbao@gmail.com>
2017-02-24 09:06:55 +0700

    Another Initial Commit

---
+++ G_Shop/G_Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G_Shop.Startup))]
+namespace G_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
