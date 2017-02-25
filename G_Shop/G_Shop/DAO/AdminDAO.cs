e3736c5ad3d3e2108823e24617215f9f3827ce6d
VMT <vominhthu0508@gmail.com>
2017-02-25 13:13:07 +0700

    them cot TenCaThe trong database, sua code Login Admin

---
+++ G_Shop/G_Shop/DAO/AdminDAO.cs
@@ -0,0 +1,17 @@
+﻿using G_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace G_Shop.DAO
+{
+    public class AdminDAO
+    {
+        GShopEntities db = null;
+        public AdminDAO()
+        {
+            db = new GShopEntities();
+        }
+
+    }
+}
