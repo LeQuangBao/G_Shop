52c9eb2a421889bd9de0319e0266edb64f923b65
VMT <vominhthu0508@gmail.com>
2017-02-25 07:24:29 +0700

    Tao Entity Framework trong folder Model, them 1 dong du lieu trong bang NguoiDung,CaThe,Loai, layout cho khach hang, sua method Login Admin

--- G_Shop/G_Shop/Areas/Admin/Controllers/HomeController.cs
+++ G_Shop/G_Shop/Areas/Admin/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using G_Shop.Models;
+using G_Shop.DAO;
 namespace G_Shop.Areas.Admin.Controllers
 {
     public class HomeController : Controller
@@ -17,9 +18,9 @@ namespace G_Shop.Areas.Admin.Controllers
         [HttpGet]
         public ActionResult Login()
         {
-            //var admin = Session["Admin"] as TMDT.Account;
-            //if (admin != null)
-            //    return RedirectToAction("Index", "Admin");
+            var admin = Session["Admin"] as NguoiDung;
+            if (admin != null)
+                return RedirectToAction("Index", "Admin");
             return View();
         }
 
@@ -29,30 +30,30 @@ namespace G_Shop.Areas.Admin.Controllers
             return RedirectToAction("Login");
         }
 
-        //[HttpPost]
-        //public ActionResult Login(string username, string pass)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var dao = new AccountDAO();
-        //        int result = dao.Login(username, pass);
-        //        if (result == 1)
-        //        {
-        //            var admin = dao.GetUser(username);
-        //            if (admin.Level == 0)
-        //            {
-        //                Session["Admin"] = admin;
-        //                return RedirectToAction("Users");
-        //            }
-        //            else
-        //            {
-        //                ModelState.AddModelError("", "Bạn không có quyền truy cập!");
-        //            }
-        //        }
-        //        else
-        //            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng. Vui lòng đăng nhập lại!");
-        //    }
-        //    return View();
-        //}
+        [HttpPost]
+        public ActionResult Login(string username, string pass)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDAO();
+                int result = dao.Login(username, pass);
+                if (result == 1)
+                {
+                    var admin = dao.GetUser(username);
+                    if (admin.VaiTro == 1)
+                    {
+                        Session["Admin"] = admin;
+                        return RedirectToAction("Users");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Bạn không có quyền truy cập!");
+                    }
+                }
+                else
+                    ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng. Vui lòng đăng nhập lại!");
+            }
+            return View();
+        }
     }
 }
