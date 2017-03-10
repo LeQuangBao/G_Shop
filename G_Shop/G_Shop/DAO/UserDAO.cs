165eb38584c4eb459845e35693b9008928b25704
Lê Quang Bảo <lequangbao@gmail.com>
2017-03-10 22:06:57 +0700

    fix checkout bug, change redirect URL for checkout
    Người mua hàng nay sẽ được redirect sang trang xem lại hóa đơn vừa đặt
    thay vì danh sách hóa đơn

--- G_Shop/G_Shop/DAO/UserDAO.cs
+++ G_Shop/G_Shop/DAO/UserDAO.cs
@@ -5,71 +5,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace G_Shop.DAO
-{
-    public class UserDAO
-    {
+namespace G_Shop.DAO {
+    public class UserDAO {
         GShopEntities1 db = null;
-        public UserDAO()
-        {
+        public UserDAO() {
             db = new GShopEntities1();
         }
-        public int Login(string username, string password)
-        {
+        public int Login(string username, string password) {
             var result = db.NguoiDungs.SingleOrDefault(x => x.TenDangNhap == username && x.MatKhau == password);
-            if (result == null)
+            if(result == null)
                 return 0;
             else
                 return 1;
         }
-        public NguoiDung GetUser(string username)
-        {
+        public NguoiDung GetUser(string username) {
             return db.NguoiDungs.SingleOrDefault(x => x.TenDangNhap == username);
         }
 
-        public List<Loai> GetAllTenLoai()
-        {
+        public List<Loai> GetAllTenLoai() {
             return db.Loais.ToList();
         }
 
-        public List<CaThe> GetAllCaThe_MaLoai(int MaLoai)
-        {
+        public List<CaThe> GetAllCaThe_MaLoai(int MaLoai) {
             return db.CaThes.Where(x => x.MaLoai == MaLoai).ToList();
         }
 
-        public List<CaThe> GetNewCaThe()
-        {
+        public List<CaThe> GetNewCaThe() {
             return db.CaThes.OrderByDescending(x => x.MaCaThe).Take(3).ToList();
         }
 
-        public string GetTenLoai_MaLoai(int MaLoai)
-        {
+        public string GetTenLoai_MaLoai(int MaLoai) {
             var model = db.Loais.Find(MaLoai);
             return model.TenLoai;
         }
 
-        public CaThe GetCaThe_MaLoai_MaCaThe(int MaLoai, int MaCaThe)
-        {
+        public CaThe GetCaThe_MaLoai_MaCaThe(int MaLoai, int MaCaThe) {
             return db.CaThes.Where(x => x.MaLoai == MaLoai && x.MaCaThe == MaCaThe).FirstOrDefault();
         }
 
-        public List<CaThe> TimCaThe(string ten)
-        {
+        public List<CaThe> TimCaThe(string ten) {
             return db.CaThes.Where(x => x.TenCaThe.Contains(ten)).ToList();
         }
 
-        public string TuoiCaThe(int MaCaThe)
-        {
+        public string TuoiCaThe(int MaCaThe) {
             string tuoi;
             var model = db.CaThes.Find(MaCaThe);
             DateDiff ngaytuoi = new DateDiff(model.NgaySinh, DateTime.Now);
-            if (ngaytuoi.Years == 0)
-                if (ngaytuoi.Months == 0)
+            if(ngaytuoi.Years == 0) {
+                if(ngaytuoi.Months == 0) {
                     tuoi = ngaytuoi.Days.ToString() + " ngày";
-                else
-                    tuoi=ngaytuoi.Months.ToString()+" tháng "+ ngaytuoi.Days.ToString() + " ngày";
-            else
-                tuoi=ngaytuoi.Years.ToString()+ " tuổi"+ngaytuoi.Months.ToString() + " tháng " + ngaytuoi.Days.ToString() + " ngày";
+                } else {
+                    tuoi = ngaytuoi.Months.ToString() + " tháng " + Math.Abs((ngaytuoi.Days - ngaytuoi.Months * 30)).ToString() + " ngày";
+                }
+            } else {
+                tuoi = ngaytuoi.Years.ToString() + " tuổi " + (ngaytuoi.Months - ngaytuoi.Years * 12).ToString() + " tháng";
+            }
             return tuoi;
         }
 
