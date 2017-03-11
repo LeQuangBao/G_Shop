b200ac57174406d16b3f1edf8b2f6b60901dde25
Lê Quang Bảo <lequangbao@gmail.com>
2017-03-11 10:44:15 +0700

    thay đổi dormat ngày tháng

--- G_Shop/G_Shop/Scripts/jsAdmin.js
+++ G_Shop/G_Shop/Scripts/jsAdmin.js
@@ -40,5 +40,11 @@ $("#btnThem").off('click').on('click', function () { //Nút thêm cá thể
     $("#form").submit();
 });
 
-
+$("input").on("change", function () {
+    this.setAttribute(
+        "data-date",
+        moment(this.value, "YYYY-MM-DD")
+        .format(this.getAttribute("data-date-format"))
+    )
+}).trigger("change")
 
