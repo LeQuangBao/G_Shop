dc9a1aabc95faa39982eb4f3d4aead556077010a
ThachLinh <nlinh6894@gmail.com>
2017-03-11 10:16:18 +0700

    sua nguoidung

--- G_Shop/G_Shop/Areas/Admin/Controllers/NguoiDungDTO.cs
+++ G_Shop/G_Shop/Areas/Admin/Controllers/NguoiDungDTO.cs
@@ -6,9 +6,9 @@ namespace G_Shop.Areas.Admin.Controllers
     {
         public int MaNguoiDung { get; set; }
         public string Email { get; set; }
-        public string GioiTinh { get; internal set; }
+        public string GioiTinh { get;  set; }
         public string MatKhau { get; set; }
-        public DateTime? NgaySinh { get; internal set; }
+        public DateTime? NgaySinh { get;  set; }
         public string SoDienThoai { get; set; }
         public string TenDangNhap { get; set; }
         public string VaiTro { get; set; }
