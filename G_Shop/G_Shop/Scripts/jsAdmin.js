98e464127c1ef5d12b05c52d5f47f6238097acf2
Lê Quang Bảo <lequangbao@gmail.com>
2017-03-13 22:49:16 +0700

    Change format date picker to "dd/mm/yyyy"

--- G_Shop/G_Shop/Scripts/jsAdmin.js
+++ G_Shop/G_Shop/Scripts/jsAdmin.js
@@ -48,3 +48,12 @@ $("input").on("change", function () {
     )
 }).trigger("change")
 
+//Bảo
+$(function () {
+    $(".custom_datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
+    $(".custom_datepicker").datepicker("option", "dateFormat", "dd/mm/yy");
+    $(".custom_datepicker").datepicker("option", "showAnim", "slideDown");
+});
