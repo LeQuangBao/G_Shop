6c13951f95a55bd0656244dcaaefed08ab91b7f0
Lê Quang Bảo <lequangbao@gmail.com>
2017-03-15 21:25:42 +0700

    insert script "custom_datepicker" for layout (user site),  Checkout now give proper "tinhTrang"

---
+++ G_Shop/G_Shop/Scripts/jsUser.js
@@ -0,0 +1,9 @@
+﻿//Bảo
+$(function () {
+    $(".custom_datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
+    $(".custom_datepicker").datepicker("option", "dateFormat", "dd/mm/yy");
+    $(".custom_datepicker").datepicker("option", "showAnim", "slideDown");
+});
