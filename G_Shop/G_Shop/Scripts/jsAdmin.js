d5063cef7fff1d04de017cd1fb7c78a7996bf191
Lê Quang Bảo <lequangbao@gmail.com>
2017-03-16 09:51:42 +0700

    datetime picker now have wide range to choose

--- G_Shop/G_Shop/Scripts/jsAdmin.js
+++ G_Shop/G_Shop/Scripts/jsAdmin.js
@@ -52,7 +52,8 @@ $("input").on("change", function () {
 $(function () {
     $(".custom_datepicker").datepicker({
         changeMonth: true,
-        changeYear: true
+        changeYear: true,
+        yearRange: "1950:2030"
     });
     $(".custom_datepicker").datepicker("option", "dateFormat", "dd/mm/yy");
     $(".custom_datepicker").datepicker("option", "showAnim", "slideDown");
--- G_Shop/G_Shop/Scripts/jsUser.js
+++ G_Shop/G_Shop/Scripts/jsUser.js
@@ -2,7 +2,8 @@
 $(function () {
     $(".custom_datepicker").datepicker({
         changeMonth: true,
-        changeYear: true
+        changeYear: true,
+        yearRange: "1950:2030"
     });
     $(".custom_datepicker").datepicker("option", "dateFormat", "dd/mm/yy");
     $(".custom_datepicker").datepicker("option", "showAnim", "slideDown");
