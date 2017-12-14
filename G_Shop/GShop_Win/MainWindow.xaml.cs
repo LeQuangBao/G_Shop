23069c3a3e62e9ce5b82e5f5a5242bb6ea061635
Thong <Thong@THONGMAP95>
2017-12-14 13:42:47 +0700

    add textbox link winform

--- G_Shop/GShop_Win/MainWindow.xaml.cs
+++ G_Shop/GShop_Win/MainWindow.xaml.cs
@@ -14,16 +14,30 @@ namespace GShop_Win
         {
             InitializeComponent();
             rfe.Click += rfe_Click;
+            go.Click += go_Click;
             //System.Windows.Controls.WebBrowser browser = new System.Windows.Controls.WebBrowser();
             //// you can put any other uri here, or better make browser field and navigate to desired uri on some event
             //browser.Navigate(new Uri("http://localhost:60181/"));
             //grdBrowserHost.Children.Add(browser);
             brw.Navigate("http://localhost:60181/");
+            link.Text = "http://localhost:60181/";
         }
         private void rfe_Click(object sender, RoutedEventArgs e)
         {
             // do something
-            //brw.Refresh();
+            brw.Refresh();
+        }
+        private void go_Click(object sender, RoutedEventArgs e)
+        {
+            // do something
+            brw.Navigate(link.Text);
+        }
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                brw.Navigate(link.Text);
+            }
         }
     }
 }
